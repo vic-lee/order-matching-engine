bcefe583f12fa86b65eb187cda4328e125a031e5
vic-lee <viclee@nyu.edu>
2018-12-09 14:46:45 -0500

    Add comment for order_spec

--- tests/orders_spec.py
+++ tests/orders_spec.py
@@ -1,3 +1,18 @@
+"""
+This file defines the keys in order data model as constants.
+
+data model for orders, arranged by traders
+{
+   "traderId": String
+   "orders":
+   [
+       "symbol": String
+       "quantity": Integer
+       "orderType": String
+  ]
+}
+"""
+
 def trader_id_key():
     return "traderId"
 
