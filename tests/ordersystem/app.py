4eb8abe002367735ec133153e1499b5d81f82605
vic-lee <viclee@nyu.edu>
2018-12-08 23:23:12 -0500

    Set up sample test

---
+++ tests/ordersystem/app.py
@@ -0,0 +1,10 @@
+import falcon
+from health_check import HealthCheck
+from orders import Orders
+
+APP = falcon.API()
+
+health_check_resource = HealthCheck()
+sample_orders = Orders()
+APP.add_route('/health', health_check_resource)
+APP.add_route('/orders', sample_orders)
