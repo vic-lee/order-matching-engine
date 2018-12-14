fb48c1cf712194bee24031c1a66f0f491d58c636
vic-lee <viclee@nyu.edu>
2018-12-14 10:08:42 -0500

    Implement test to verify if data is posted successfully

--- test/tests/test_orders.py
+++ test/tests/test_orders.py
@@ -46,6 +46,21 @@ class TestOrdersPost(TestOrders):
             self.assertIn(spec.post_success_resp_msg, resp["Message"])
             self.assertEqual(falcon.HTTP_201, result.status)
 
+            trader_id = payload[spec.data_key][spec.trader_id_key]
+            orders_to_post = payload[spec.data_key][spec.order_key]
+
+            param = {"trader_id": trader_id}
+            verify_data = self.simulate_get(self.order_endpoint, params=param)
+            resp = verify_data.json
+            for order in resp[spec.data_key]:
+                self.assertIn(spec.order_symbol_key, order)
+                self.assertIn(spec.order_quantity_key, order)
+                self.assertIn(spec.order_type_key, order)
+                self.assertIn(spec.order_status_key, order)
+                self.assertIn(spec.order_time_key, order)
+            for order in orders_to_post:
+                self.assertOrderExists(order, resp[spec.data_key])
+
         elif test_type == testdata.wrong_json_format_post:
             self.assertIn(spec.resp_msg_key, resp)
             self.assertEqual(spec.post_json_missing_key_err_msg, resp["Message"])
