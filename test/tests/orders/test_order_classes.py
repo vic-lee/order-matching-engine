fd85c6ee33baa2db381e7fea2b0d60a15b96d314
vic-lee <viclee@nyu.edu>
2018-12-14 10:17:05 -0500

    Refactor getting and matching orders to new files

--- test/tests/orders/test_order_classes.py
+++ test/tests/orders/test_order_classes.py
@@ -7,7 +7,6 @@ import tests.orders.test_orders_data as testdata
 import orders.spec as spec
 
 class TestBase(testing.TestCase):
-
     def setUp(self):
         super(TestBase, self).setUp()
         self.app = app.create()
@@ -28,79 +27,6 @@ class TestOrders(TestBase):
     order_endpoint = '/orders'
 
 
-
-class TestOrdersGet(TestOrders):
-
-    def test_get_order(self):
-        self.get_order_test_handler(\
-            testdata.std_post_data,\
-            testdata.std_post_data[spec.data_key][spec.trader_id_key])
-
-        multiple_post_payload = [testdata.std_post_data,\
-            testdata.std_post_data_2, testdata.std_post_data_3]
-
-        self.get_order_test_handler(multiple_post_payload)
-
-
-    def get_order_test_handler(self, payload, trader_id=None):
-        if (type(payload) == list):
-            for p in payload:
-                self.simulate_post('/orders', json=p)
-        else:
-            self.simulate_post('/orders', json=payload)
-
-        if trader_id == None:
-            self.handle_get_all_trade_orders(payload)
-        else:
-            self.handle_get_trader_order(trader_id, payload[spec.data_key][spec.order_key])
-
-
-    def handle_get_trader_order(self, trader_id, data_sent):
-        param = {"trader_id": trader_id}
-        result = self.simulate_get(self.order_endpoint, params=param)
-        resp = result.json
-        self.assertEqual(falcon.HTTP_200, result.status)
-        self.assertIn(spec.data_key, resp)
-        for order in data_sent:
-            self.assertOrderExists(order, resp[spec.data_key])
-
-
-    def handle_get_all_trade_orders(self, data_sent):
-        result = self.simulate_get(self.order_endpoint)
-        resp = result.json
-        self.assertEqual(falcon.HTTP_200, result.status)
-        if spec.data_key not in resp:
-            '''Expect getting order data from multiple traders'''
-            for payload in data_sent:
-                trader_id = payload[spec.data_key][spec.trader_id_key]
-                for order in payload[spec.data_key][spec.order_key]:
-                    self.assertOrderExists(order, resp[trader_id][spec.order_key])
-        else:
-            '''Expect getting data from one trading account'''
-            for order_item in data_sent:
-                self.assertOrderExists(order_item, resp[spec.data_key])
-
-
-class TestOrdersMatching(TestOrders):
-    def test_order_matching(self):
-        self.order_matching_test_handler(testdata.order_matching_data)
-
-    def order_matching_test_handler(self, payload):
-        """
-        Edge case warning: if any order in order_matching_data has a
-        matching order from previous tests, the matching engine may
-        fill that order instead of the one tested hereself.
-        For better testability, consider data from API before this test.
-        """
-        for p in payload:
-            self.simulate_post(self.order_endpoint, json=p)
-        result = self.simulate_get(self.order_endpoint)
-        resp = result.json
-        self.assertEqual(falcon.HTTP_200, result.status)
-        for p in payload:
-            trader_id = p[spec.data_key][spec.trader_id_key]
-            self.assertEqual(spec.order_status_filled,\
-             resp[trader_id][spec.order_key][0][spec.order_status_key])
 
 # if __name__ == "__main__":
 #     unittest.main()
