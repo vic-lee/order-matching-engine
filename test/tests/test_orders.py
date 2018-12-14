9b254d32872a9433226aad94795036dcf3b1b174
vic-lee <viclee@nyu.edu>
2018-12-13 21:29:56 -0500

    Implement posting test

--- test/tests/test_orders.py
+++ test/tests/test_orders.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from falcon import testing
 import app
 
@@ -9,12 +10,43 @@ class TestOrders(testing.TestCase):
         self.app = app.create()
 
 class TestOrderCreation(TestOrders):
+
     def test_get_message(self):
-        doc = {"Message": "No order"}
         result = self.simulate_get('/orders')
-        print(doc)
-        print(result)
-        self.assertEqual(result.json, doc)
+        # for individual_resp in resp:
+            # assert "orders" in individual_resp
+
+
+    def test_post_order(self):
+        docs = {
+            "data":
+            {
+                "traderId": "skbks-sdk39sd-3ksfl43io3-alkjasf-34",
+                "orders":
+                [
+                    {
+                        "symbol": "AAPL",
+                        "quantity": 100,
+                        "orderType": "buy"
+                    },
+                    {
+                        "symbol": "NVDA",
+                        "quantity": 5000,
+                        "orderType": "buy"
+                    },
+                    {
+                        "symbol": "MSFT",
+                        "quantity": 2500,
+                        "orderType": "sell"
+                    }
+                ]
+            }
+        }
+        headers = {"Content-Type": "application/json"}
+        result = self.simulate_post('/orders', json=docs)
+        resp = result.json
+        assert "Message" in resp
+        assert "Post successful" in resp["Message"]
 
 # if __name__ == "__main__":
 #     unittest.main()
