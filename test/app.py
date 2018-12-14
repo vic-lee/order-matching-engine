03345ae32dd5e044e65be54745978ac061949f43
vic-lee <viclee@nyu.edu>
2018-12-13 20:18:30 -0500

    Implement Create function for API testing

--- test/app.py
+++ test/app.py
@@ -2,13 +2,14 @@ import falcon
 from health_check import HealthCheck
 from orders.orders import OrderResources
 
-APP = falcon.API()
 
 health_check_resource = HealthCheck()
 order_resources = OrderResources()
 
-APP.add_route('/health', health_check_resource)
-APP.add_route('/orders', order_resources)
-
 def create():
-    return falcon.API()
+    api = falcon.API()
+    api.add_route('/health', health_check_resource)
+    api.add_route('/orders', order_resources)
+    return api
+
+APP = create()
